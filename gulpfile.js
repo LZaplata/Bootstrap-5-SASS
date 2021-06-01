66e5b86bd881968d7a21f41d4f1aea41c9b85953
zap <zap@hucr.cz>
2021-06-01 11:24:46 +0200

    minor changes in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
-const {src, dest, watch, parallel, start} = require("gulp");
-const sass = require("gulp-sass");
-const minify = require("gulp-clean-css");
+let {src, dest, watch, parallel} = require("gulp");
+let sass = require("gulp-sass");
+let minify = require("gulp-clean-css");
+let themeDir = "./themes/lzaplata-bootstrap-5-sass";
 
 function sassTask() {
-    return src("assets/sass/theme.sass")
-        .pipe(sass({outputStyle: "compressed"}))
-        .pipe(dest("assets/css"));
+    return src(themeDir + "/assets/sass/theme.sass")
+        .pipe(sass({outputStyle: "compressed", includePaths: ["./node_modules"]}))
+        .pipe(dest(themeDir + "/assets/css"));
 }
 
 function cssTask() {
     return src("node_modules/bootstrap-icons/font/bootstrap-icons.css")
         .pipe(minify())
-        .pipe(dest("assets/css"))
+        .pipe(dest(themeDir + "/assets/css"))
 }
 
 function fontsTask() {
     return src("node_modules/bootstrap-icons/font/fonts/*")
-        .pipe(dest("assets/css/fonts"))
+        .pipe(dest(themeDir + "/assets/css/fonts"))
 }
 
 function jsTask() {
@@ -25,13 +26,13 @@ function jsTask() {
         "node_modules/bootstrap/dist/js/bootstrap.js",
         "node_modules/@popperjs/core/dist/umd/popper.js"
     ])
-        .pipe(dest("assets/js"))
+        .pipe(dest(themeDir + "/assets/js"))
 }
 
 function watchTask() {
     watch([
-        "assets/**/theme.sass",
-        "assets/**/_variables.sass",
+        themeDir + "assets/**/theme.sass",
+        themeDir + "assets/**/_variables.sass",
     ], {ignoreInitial: false}, sassTask);
 }
 
